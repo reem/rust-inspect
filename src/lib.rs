e7bfdd2f50bdbccee50b421fd2512c8b5a8eb016
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-04 20:32:08 -0700

    (feat) Added debug macro.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
-#![crate_id = ""]
+#![crate_id = "line_debug"]
 #![deny(missing_doc)]
+#![feature(macro_rules)]
 
-//! Documentation goes here.
+//! A lightweight library for quickly debugging rust code.
+
+//! Logs the file, line number, and expressions along with what they equal.
+//!
+//! For instance:
+//!
+//! let a = 7;
+//! debug!(a, a + 4) => file.rs - 2: a = 7, a + 4 = 11
+#[macro_export]
+macro_rules! debug(
+    ($($a:expr),*) => {
+        println!(
+            "{} - {}: {}",
+            file!(), line!(),
+            format!(
+                concat!($(stringify!($a), " = {:?}, "),*), $($a),*
+            )
+        );
+    }
+)
 
