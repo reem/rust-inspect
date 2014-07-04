b4ed0fb9c9f1ec1a511b1f6206e70d766456749d
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-04 00:29:57 -0700

    (update) Updated for latest rustc.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,11 +1,11 @@
 #![feature(phase)]
 
 extern crate debug;
-#[phase(syntax, link)]
+#[phase(plugin, link)]
 extern crate inspect;
 
 fn main() {
-    let a = 7;
+    let a = 7u;
     inspect!(a, a + 4, a - 3);
     // Logs: "hello.rs - 9: a = 7, a + 4 = 11, a - 3 = 4"
 }
